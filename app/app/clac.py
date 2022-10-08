b7c28a0630569c34eefb913b316f59caed8d9dec
sazzadhossain881 <77628174+sazzadhossain881@users.noreply.github.com>
2022-10-08 17:23:22 +0600

    inatall posgresql database adaptor

---
+++ app/app/clac.py
@@ -0,0 +1,9 @@
+"""calculator functions"""
+
+def add(x,y):
+    """add x and y and return result"""
+    return x+y
+
+def subtract(x, y):
+    """subtract x from y and return result"""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""simple test"""
+
+from django.test import SimpleTestCase
+from app import clac
+
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        """test adding number together"""
+        res = clac.add(5,6)
+
+        self.assertEqual(res, 11)
+    
+    def test_subtract_numbers(self):
+
+        res = clac.subtract(15, 10)
+
+        self.assertEqual(res, 5)
