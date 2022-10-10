20fbecdb60a82b10e53e22826b93b07f3693b57a
sazzadhossain881 <77628174+sazzadhossain881@users.noreply.github.com>
2022-10-10 11:26:41 +0600

    wait_for_db commands

--- app/app/clac.py
+++ app/app/clac.py
@@ -1,8 +1,10 @@
 """calculator functions"""
 
-def add(x,y):
+
+def add(x, y):
     """add x and y and return result"""
     return x+y
+
 
 def subtract(x, y):
     """subtract x from y and return result"""
--- app/app/tests.py
+++ app/app/tests.py
@@ -5,13 +5,12 @@ from app import clac
 
 
 class CalcTests(SimpleTestCase):
-
     def test_add_numbers(self):
         """test adding number together"""
-        res = clac.add(5,6)
+        res = clac.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
 
         res = clac.subtract(15, 10)
