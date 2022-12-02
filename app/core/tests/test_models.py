7cb4b68736843d724ebf8ff90fa3faf599b3ad6f
sazzadhossain881 <77628174+sazzadhossain881@users.noreply.github.com>
2022-12-02 18:33:49 +0600

    implementing filter for recipe

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,15 @@
 """Tests for models"""
 
+from decimal import Decimal
+from unittest.mock import patch
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def create_user(email='user@example.com', password='testpass123'):
+    """Test and create a new user"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -44,3 +52,46 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successfull"""
+
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='simple recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='sample recipe description',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successfull"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredients(self):
+        """Test creating an ingredient is successfull"""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredients1'
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test generating image path"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
